b5d4b6770afd02eae31f3835ec4173b27c83d784
vagrant <vagrant@localhost.localdomain>
2020-08-16 11:44:38 +0000

    devise修正/バリデーション設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,42 @@
 class ApplicationController < ActionController::Base
+    # before_actionでログインしないと他のページにアクセスできないようにする設定
+    # before_action :authenticate_user!
+    # before_action :authenticate_admin!
+    
+    # protected内の「configure_permitted_parameters」を設定
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  # アカウント登録後のリダイレクト先（とりあえずrootにしてます。）
+    def after_sign_up_path_for(resource)
+        root_path
+    end
+    
+    # ログイン後のリダイレクト先（とりあえずrootにしてます。）
+    def after_sign_in_path_for(resource)
+        root_path
+    end
+    
+    
+    # ログアウト後のリダイレクト先
+    def after_sign_out_path_for(resource)
+        root_path
+    end 
+
+  
+    protected
+        # deviseで使えるパラメータを許可する
+        def configure_permitted_parameters
+            devise_parameter_sanitizer.permit(:sign_up, keys: [
+                :email, 
+                :last_name_kana,
+                :first_name_kana,
+                :last_name,
+                :first_name,
+                :postal_code,
+                :street_address,
+                :phone_num,
+                :is_deleted
+            ])
+        end
+
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,13 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  # 会員バリデーション
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :postal_code, presence: true
+  validates :street_address, presence: true
+  validates :phone_num, presence: true
 end
