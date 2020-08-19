d2161c5d2ca68082cfa1c6b279c55dbab9b43e25
vagrant <vagrant@localhost.localdomain>
2020-08-19 05:28:49 +0000

    管理者top修正

--- app/controllers/admins/home_controller.rb
+++ app/controllers/admins/home_controller.rb
@@ -2,5 +2,6 @@ class Admins::HomeController < ApplicationController
     before_action :authenticate_admin!
     
     def top
+        @today_orders = Order.where(created_at: Time.zone.now.all_day).page(params[:page])
     end
 end
--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module NaganoCake
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    
+    # タイムゾーンを東京 日本語化対応
+    config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
 
     # Settings in config/environments/* take precedence over those specified here.
