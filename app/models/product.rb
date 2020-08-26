63a83624148b2b19168c3933e2202c7eab7e28e1
vagrant <vagrant@localhost.localdomain>
2020-08-26 02:30:34 +0000

    注文/製作ステータス

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,7 +8,7 @@ class Product < ApplicationRecord
 
 	validates :name, presence: true, length: {minimum: 1, maximum:15}
 	validates :introduction, presence: true
-	# validates :image, presence: true
+	validates :image, presence: true
 	validates :genre_id, presence: true
 	validates :price, numericality: true
 	validates :is_active, inclusion: {in: [true, false]}
