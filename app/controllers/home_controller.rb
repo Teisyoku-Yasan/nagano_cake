02840280a968f9f79363a765e2a5870fafcdd26c
vagrant <vagrant@localhost.localdomain>
2020-08-28 08:22:56 +0000

    topページ/ジャンル検索機能・おすすめ商品表示

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
     
     def top
+    	@product = Product.includes(:genre).where(genres: {is_active: true}).where(is_active: true)
+    	@genres = Genre.where(is_active: true)
+    	@all_ranks = Product.find(OrderDetail.group(:product_id).order('count(product_id) desc').limit(4).pluck(:product_id))
     end
 
     def about
